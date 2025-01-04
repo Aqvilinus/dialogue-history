ff048a08b82b62dcec1bdc28649d99987dba536f
Norviah <21983700+Norviah@users.noreply.github.com>
2025-01-04 04:44:06 -0800

    build(scripts/import): delete all mod files before importing

--- scripts/import.sh
+++ scripts/import.sh
@@ -2,6 +2,9 @@
 
 source "$(git rev-parse --show-toplevel)/scripts/utils.sh"
 
+# delete mod files from the git directory
+source "$(git rev-parse --show-toplevel)/scripts/delete.sh"
+
 # copy mod files to the git directory
 cp -r "${paths[CYBERPUNK_ROOT]}/${paths[REDSCRIPT_PATH]}/${paths[NAME]}/" "${paths[ROOT]}/redscript"
 cp -r "${paths[CYBERPUNK_ROOT]}/${paths[INPUT_PATH]}/${paths[NAME]}.xml" "${paths[ROOT]}/input/${paths[NAME]}.xml"
